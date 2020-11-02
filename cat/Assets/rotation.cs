06d55963b399c7bbb0e73d3c10e73716c048f2cf
Janet-yu0219 <71627350+Janet-yu0219@users.noreply.github.com>
2020-11-02 14:15:09 +0800

    Add files via upload

---
+++ cat/Assets/rotation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotation : MonoBehaviour
+{
+    public iTween.EaseType easeType;
+    public iTween.LoopType loopType;
+
+
+    void Start()
+    {
+        iTween.RotateTo(this.gameObject, iTween.Hash("z", 720, "time", 800f, "easeType", easeType, "loopType", loopType));
+    }
+
+}
